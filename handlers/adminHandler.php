7f80948bcf8ddea8a64e7f0b00ff67fdbe193e10
met5678 <andrew@luminal.io>
2015-01-21 00:19:44 -0500

    Removing another old artifact

--- handlers/adminHandler.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('../inc/db_functions.php');dbConnect();
-$actionRequest = $_POST["action"];
-
-if(!is_null($actionRequest)) {
-	switch($actionRequest) {
-		case "resetAliases":
-			repopulateAliases();			break;	}}dbClose();?>

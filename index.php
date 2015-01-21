5f7853a10f03d9144065b326fe64ed1c701e1bce
met5678 <andrew@luminal.io>
2015-01-20 22:42:47 -0500

    Textuality fix

--- index.php
+++
@@ -1,10 +0,0 @@
-<html><head><link rel="stylesheet" type="text/css" href="/textuality/css/textuality_base.css" /><link rel="stylesheet" type="text/css" href="/textuality/css/textuality.css" /><script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script><script></script></head><body><div id="textuality">	<div class="main">		<?php include 'modules/mainArea.php'; ?>	</div>
-	<div class="header">
-	<img class="header-logoImage" src="img/main_logo.png" />			<div class="header-phoneNumber">571-989-4785</div>
-	</div>
-	<div class="footer">
-		<?php include 'modules/schedule.php'; ?>
-	</div>
-</div>
-</body>
-</html>

d91e61d4eec561fd794203d0345cccc3042e72e0
met5678 <andrew@luminal.io>
2015-01-22 03:13:09 -0500

    Restoring old test code for now

--- textuality-app/server/cloudinary.js
+++ textuality-app/server/cloudinary.js
@@ -1,2 +1,32 @@
+// Write your package code here!
 Cloudinary = {};
 
+/*var uploadOptions = {
+	detection:'rekognition_face'
+};
+
+var cloudinary = Npm.require('cloudinary');
+cloudinary.configure({
+
+});
+
+var wrappedUpload = Meteor.wrapAsync(function(url,options,cb) {
+	cloudinary.uploader.upload(url, function(result) {
+		cb(result);
+	},options);
+});
+
+
+
+Cloudinary.uploadImage = function(url,options) {
+	var options = {
+		detection:'rekognition_face'
+	};
+	var result = wrappedUpload(url);
+
+
+};
+
+Cloudinary.detectFace = function(url) {
+	var result = wrapped
+}*/

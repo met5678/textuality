295e70b86943d5eeac336e57337d2043f96f1947
Andrew Metcalf <met5678@gmail.com>
2023-11-08 12:58:47 -0500

    Much nicer spinning sounds

--- textuality-client/imports/ui/modules/SlotMachine/SlotMachineSounds.tsx
+++ textuality-client/imports/ui/modules/SlotMachine/SlotMachineSounds.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SlotMachineWithHelpers } from '/imports/api/themes/casino/slotMachines/slotMachines';
 
 const SlotMachineSounds = ({
@@ -7,9 +7,19 @@ const SlotMachineSounds = ({
   slotMachine: SlotMachineWithHelpers;
 }) => {
   const { status } = slotMachine;
+  const [spinOption, setSpinOption] = useState(1);
+  useEffect(() => {
+    setSpinOption(Math.round(Math.random()) + 1);
+  }, [status]);
 
   if (status === 'spinning')
-    return <audio src="/casino/sounds/slots-spinning-loop.ogg" autoPlay loop />;
+    return (
+      <audio
+        src={`/casino/sounds/slots-spinning-${spinOption}.ogg`}
+        autoPlay
+        loop
+      />
+    );
   if (status === 'win-normal')
     return <audio src="/casino/sounds/slots-win.ogg" autoPlay />;
   if (status === 'lose')

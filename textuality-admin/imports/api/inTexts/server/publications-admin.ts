e1279ee9117da9006f78f4651cb530131a201d48
Andrew Metcalf <met5678@gmail.com>
2023-11-11 15:41:20 -0500

    Limiting amount of texts sent to admin

--- textuality-admin/imports/api/inTexts/server/publications-admin.ts
+++ textuality-admin/imports/api/inTexts/server/publications-admin.ts
@@ -5,7 +5,12 @@ import Events from '/imports/api/events';
 import getPaginatedCursor from '../../_utils/publish-paginated';
 
 Meteor.publish('inTexts.all', function () {
-  this.autorun(() => InTexts.find({ event: Events.currentId()! }));
+  this.autorun(() =>
+    InTexts.find(
+      { event: Events.currentId()! },
+      { sort: { time: -1 }, limit: 100 },
+    ),
+  );
 });
 
 Meteor.publish('inTexts.paged', getPaginatedCursor(InTexts));
--- textuality-admin/imports/api/outTexts/server/publications-admin.js
+++ textuality-admin/imports/api/outTexts/server/publications-admin.js
@@ -3,6 +3,11 @@ import { Meteor } from 'meteor/meteor';
 import OutTexts from '../';
 import Events from 'api/events';
 
-Meteor.publish('outTexts.all', function() {
-  this.autorun(() => OutTexts.find({ event: Events.current()._id }));
+Meteor.publish('outTexts.all', function () {
+  this.autorun(() =>
+    OutTexts.find(
+      { event: Events.current()._id },
+      { sort: { time: -1 }, limit: 100 },
+    ),
+  );
 });

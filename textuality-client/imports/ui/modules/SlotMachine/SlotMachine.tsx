15e471d02bc94180663cfa1840017fb25623dcc2
Jen Gorski <yjgorski18@gmail.com>
2024-11-15 11:35:36 -0500

    use condensed and compressed for slot titles

--- textuality-client/imports/ui/modules/SlotMachine/SlotMachine.tsx
+++ textuality-client/imports/ui/modules/SlotMachine/SlotMachine.tsx
@@ -64,6 +64,11 @@ const SlotMachine = ({ slotMachine, skin }: SlotMachineProps) => {
     win: showWin,
   });
 
+  const titleClasses = classNames(
+    'title-name',
+    name.length > 8 ? 'compressed' : name.length > 6 ? 'condensed' : '',
+  );
+
   const items =
     skin === 'space'
       ? [
@@ -92,7 +97,7 @@ const SlotMachine = ({ slotMachine, skin }: SlotMachineProps) => {
         }}
       >
         <div className="title-container">
-          <div className="title-name">{name}</div>
+          <div className={titleClasses}>{name}</div>
 
           <div className="instrux-area">
             <div className="title-tospin">TO SPIN:</div>

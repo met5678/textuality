33098c9c5aea5effc6b399b4fd09f14e5576d015
Andrew Metcalf <met5678@gmail.com>
2025-03-18 20:58:45 -0400

    Delete zrefactor folder

--- zrefactor-admin-material/textuality-admin/imports/api/in-texts/collection.js
+++
@@ -1,9 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-import InTextSchema from '../schemas/inText';
-
-const InTexts = new Mongo.Collection('inTexts');
-
-InTexts.attachSchema(InTextSchema);
-
-export default InTexts;

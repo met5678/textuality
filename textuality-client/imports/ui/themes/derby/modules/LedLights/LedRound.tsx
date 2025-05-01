30dc340bf0a6eb76f8f44c4aa19c212008d05abd
Jen Gorski <jgorski18@gmail.com>
2025-04-30 23:43:45 -0400

    center the icon

--- textuality-client/imports/ui/themes/derby/modules/LedLights/LedRound.tsx
+++ textuality-client/imports/ui/themes/derby/modules/LedLights/LedRound.tsx
@@ -21,25 +21,36 @@ export const LedRound: React.FC<{
   style = {},
 }) => {
   const lightSize = frame ? size - 8 : size;
-  return (
-    <div className={'derby-led-wrapper'}>
-      {frame && (
-        <div
-          className={'derby-led-frame'}
-          style={{ width: `${size}px`, height: `${size}px` }}
-        />
-      )}
-      <div
-        className={`derby-led led-round
+
+  const ledLight = (
+    <div
+      className={`derby-led led-round
           ${color ? `derby-led-${color}` : ''} 
           ${!icon ? 'led-fill' : ''}
           ${off ? 'led-off' : ''} 
           ${blink ? 'led-blink' : ''} 
           ${className}`}
-        style={{ width: `${lightSize}px`, height: `${lightSize}px`, ...style }}
-      >
-        {icon && <div className="led-icon">{icon}</div>}
-      </div>
+      style={{
+        width: `${lightSize}px`,
+        height: `${lightSize}px`,
+        ...style,
+      }}
+    >
+      {icon && <div className="led-icon">{icon}</div>}
+    </div>
+  );
+  return (
+    <div className={'derby-led-wrapper'}>
+      {frame ? (
+        <div
+          className={'derby-led-frame'}
+          style={{ width: `${size}px`, height: `${size}px` }}
+        >
+          {ledLight}
+        </div>
+      ) : (
+        ledLight
+      )}
     </div>
   );
 };

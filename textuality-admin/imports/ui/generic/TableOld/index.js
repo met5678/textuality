fbe3c2436fc67007caca9dacc5b046e6c7fa8df4
Andrew Metcalf <met5678@gmail.com>
2025-05-24 10:53:10 -0400

    Removing TableOld, creating code workspace

--- textuality-admin/imports/ui/generic/TableOld/index.js
+++
@@ -1,8 +0,0 @@
-import Table from './Table';
-import TableInsertModal from './TableInsertModal';
-import TableOrderCell from './TableOrderCell';
-import generateObjColumn from './generateObjColumn';
-
-export default Table;
-
-export { Table, TableInsertModal, TableOrderCell, generateObjColumn };

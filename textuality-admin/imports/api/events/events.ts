609c73219e3398ced4a86264cc1e14d606b57941
Andrew Metcalf <met5678@gmail.com>
2026-09-14 07:37:15 -0700

    Adding async event utils

--- textuality-admin/imports/api/events/events.ts
+++ textuality-admin/imports/api/events/events.ts
@@ -7,6 +7,11 @@ import {
   currentId,
   currentIdOrThrow,
   allIds,
+  allIdsAsync,
+  currentAsync,
+  currentIdAsync,
+  currentIdOrThrowAsync,
+  currentOrThrowAsync,
 } from './helpers';
 
 interface EventsCollection extends Mongo.Collection<Event> {
@@ -15,11 +20,27 @@ interface EventsCollection extends Mongo.Collection<Event> {
   currentId: () => EventId | undefined;
   currentIdOrThrow: () => EventId;
   allIds: () => EventId[];
+  currentAsync: () => Promise<Event | undefined>;
+  currentOrThrowAsync: () => Promise<Event>;
+  currentIdAsync: () => Promise<EventId | undefined>;
+  currentIdOrThrowAsync: () => Promise<EventId>;
+  allIdsAsync: () => Promise<EventId[]>;
 }
 
 const Events: EventsCollection = Object.assign(
   new Mongo.Collection<Event>('events'),
-  { current, currentOrThrow, currentId, currentIdOrThrow, allIds },
+  {
+    current,
+    currentOrThrow,
+    currentId,
+    currentIdOrThrow,
+    allIds,
+    currentAsync,
+    currentOrThrowAsync,
+    currentIdAsync,
+    currentIdOrThrowAsync,
+    allIdsAsync,
+  },
 );
 
 Events.attachSchema(EventSchema);
--- textuality-admin/imports/api/events/helpers.ts
+++ textuality-admin/imports/api/events/helpers.ts
@@ -2,12 +2,12 @@ import { Meteor } from 'meteor/meteor';
 import Events from './events';
 import { Event, EventId } from '/imports/schemas/event';
 
-const current = (): Event | undefined => {
+export const current = (): Event | undefined => {
   const events = Events.find({ active: true }).fetch();
   return events[0];
 };
 
-const currentOrThrow = (): Event => {
+export const currentOrThrow = (): Event => {
   const current = Events.current();
   if (!current) {
     throw new Meteor.Error('event-not-found', 'Event not active');
@@ -15,20 +15,46 @@ const currentOrThrow = (): Event => {
   return current;
 };
 
-const currentId = (): EventId | undefined => {
+export const currentId = (): EventId | undefined => {
   const currentEvent = current();
   return currentEvent ? currentEvent._id : undefined;
 };
 
-const currentIdOrThrow = (): EventId => {
+export const currentIdOrThrow = (): EventId => {
   const current = currentOrThrow();
   return current._id;
 };
 
-const allIds = (): EventId[] => {
+export const allIds = (): EventId[] => {
   return Events.find({}, { fields: { _id: 1 } })
     .fetch()
     .map((event) => event._id);
 };
 
-export { current, currentOrThrow, currentId, currentIdOrThrow, allIds };
+export const currentAsync = async (): Promise<Event | undefined> => {
+  const events = await Events.find({ active: true }).fetchAsync();
+  return events[0];
+};
+
+export const currentOrThrowAsync = async (): Promise<Event> => {
+  const current = await currentAsync();
+  if (!current) {
+    throw new Meteor.Error('event-not-found', 'Event not active');
+  }
+  return current;
+};
+
+export const currentIdAsync = async (): Promise<EventId | undefined> => {
+  const currentEvent = await currentAsync();
+  return currentEvent ? currentEvent._id : undefined;
+};
+
+export const currentIdOrThrowAsync = async (): Promise<EventId> => {
+  const current = await currentOrThrowAsync();
+  return current._id;
+};
+
+export const allIdsAsync = async (): Promise<EventId[]> => {
+  const events = await Events.find({}, { fields: { _id: 1 } }).fetchAsync();
+  return events.map((event) => event._id);
+};

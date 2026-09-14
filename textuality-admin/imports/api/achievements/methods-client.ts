52e8c4bfcefdbceec0712b716055df699b2f62be
Andrew Metcalf <met5678@gmail.com>
2026-09-14 07:37:08 -0700

    Achievements method conversion

--- textuality-admin/imports/api/achievements/methods-client.ts
+++ textuality-admin/imports/api/achievements/methods-client.ts
@@ -1,107 +1,19 @@
 import { Meteor } from 'meteor/meteor';
-import { onlyEmoji } from 'emoji-aware';
 
-import Achievements from './achievements';
-import AchievementUnlocks from '/imports/api/achievementUnlocks';
 import Players from '/imports/api/players';
-import Media from '/imports/api/media';
-import Events from '/imports/api/events';
-import { Achievement } from '/imports/schemas/achievement';
 import { InText } from '/imports/schemas/inText';
-import { powerupsGeneratePowerup } from '../themes/derby/powerups/methods/powerups.generatePowerup';
-import { raceAwardLogicClue } from '../themes/derby/race/logic-clues/races.awardLogicClue';
+
+import './methods-client/achievements.tryUnlock';
+import { tryUnlockAchievement } from './methods-client/achievements.tryUnlock';
 
 Meteor.methods({
-  'achievements.tryUnlock': ({
-    trigger,
-    trigger_detail_number,
-    trigger_detail_string,
-    playerId,
-  }) => {
-    const achievementQuery: Partial<Achievement> = {
-      event: Events.currentId()!,
-      trigger,
-    };
-    typeof trigger_detail_number === 'number' &&
-      (achievementQuery.trigger_detail_number = trigger_detail_number);
-    typeof trigger_detail_string === 'string' &&
-      (achievementQuery.trigger_detail_string = trigger_detail_string);
-
-    const achievements = Achievements.find(achievementQuery).fetch();
-
-    let earnedAchievement = false;
-    if (achievements.length) {
-      const player = Players.findOne(playerId);
-      if (!player) return;
-      const playerAchievements = AchievementUnlocks.find(
-        { event: Events.currentId(), player: playerId },
-        { fields: { achievement: 1 } },
-      ).map((unlock) => unlock.achievement);
-
-      achievements
-        .filter((achievement) => !playerAchievements.includes(achievement._id))
-        .forEach((achievement, i) => {
-          AchievementUnlocks.insert({
-            event: Events.currentIdOrThrow(),
-            achievement: achievement._id,
-            name: achievement.name,
-            time: new Date(),
-            player: playerId,
-            alias: player.alias,
-            avatar: player.avatar!,
-            numAchievements: playerAchievements.length + 1 + i,
-          });
-          Achievements.update(achievement._id!, { $inc: { earned: 1 } });
-          Players.update(playerId, { $inc: { numAchievements: 1 } });
-
-          if (achievement.money_award) {
-            Meteor.call('players.giveMoney', {
-              playerId: player._id,
-              money: achievement.money_award,
-            });
-            player.money += achievement.money_award;
-          }
-
-          if (achievement.quest_award_type !== 'NONE') {
-            Meteor.call('quests.startQuestOfType', {
-              playerId: player._id,
-              type: achievement.quest_award_type,
-            });
-          }
-
-          if (achievement.derby_award !== 'NONE') {
-            if (achievement.derby_award === 'HORSE_POWERUP') {
-              powerupsGeneratePowerup(player._id);
-            }
-            if (achievement.derby_award === 'RACE_RESULT_LOGIC') {
-              raceAwardLogicClue(player._id);
-            }
-          }
-
-          if (achievement.player_text?.trim()) {
-            Meteor.call('autoTexts.sendCustom', {
-              playerText: achievement.player_text,
-              playerId,
-              mediaUrl: achievement.player_text_image ?? undefined,
-              source: 'achievement',
-              templateVars: {
-                money_award: achievement.money_award,
-              },
-            });
-          }
-          earnedAchievement = true;
-        });
-      return earnedAchievement;
-    }
-  },
-
-  'achievements.checkAfterInText': (inText: InText) => {
+  'achievements.checkAfterInText': async (inText: InText) => {
     const playerId = inText.player;
-    const player = Players.findOne(playerId);
+    const player = await Players.findOneAsync(playerId);
     if (!player) return;
 
     if (player.status === 'active') {
-      Meteor.call('achievements.tryUnlock', { playerId, trigger: 'JOINED' });
+      tryUnlockAchievement({ playerId, trigger: 'JOINED' });
     }
 
     // if (['feed', 'mediaOnly'].includes(inText.purpose)) {
